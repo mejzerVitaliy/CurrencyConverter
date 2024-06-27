595bd207a6aaf4ef7b33b97a1a5133be98385cf9
mejzerVitaliy <mejzervitalik@gmail.com>
2024-06-27 12:13:21 +0300

    day 1/ 27.06.24     today i made a theme switcher

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-const lightMode = document.getElementById('lightMode')
 
-lightMode.addEventListener('click', () => {
-    lightMode.innerText = 'dark mode'
-    lightMode.style.backgroundColor = 'black'
-    lightMode.style.color = 'white'
-})
+const body = document.body;
+const lightModeBtn = document.getElementById('lightModeBtn')
+const headAndFoot = document.querySelectorAll('#headAndFoot')
+const converter = document.querySelector('.converter')
+
+lightModeBtn.addEventListener('click', themeSwitcher)
+
+async function themeSwitcher() {
+    if (lightModeBtn.innerText === "light mode") {
+        lightModeBtn.innerText = "dark mode"
+        lightModeBtn.classList.remove('lightModeBtn')
+        lightModeBtn.classList.add('darkModeBtn')
+
+        body.style.color = 'var(--darkThemeColor)'
+        
+        headAndFoot.forEach(item => {
+            item.style.backgroundColor = 'var(--darkThemeBgColor)'
+        })
+        converter.style.backgroundColor = 'var(--darkConverterBgColor)'
+    
+    } else if (lightModeBtn.innerText === "dark mode") {
+        lightModeBtn.innerText = "light mode"
+        lightModeBtn.classList.remove('darkModeBtn')
+        lightModeBtn.classList.add('lightModeBtn')
+
+        body.style.color = 'var(--lightThemeColor)'
+        headAndFoot.forEach(item => {
+            item.style.backgroundColor = 'var(--lightThemeBgColor)'
+        })
+        converter.style.backgroundColor = 'var(--lightConverterBgColor)'
+    }
+}
