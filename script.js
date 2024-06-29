05c14cd49c62abadb10ff5a223fb02ed79ef622c
mejzerVitaliy <mejzervitalik@gmail.com>
2024-06-29 20:18:15 +0300

    Day #3, 29.06, i made a logic of the converter

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 
 const body = document.body;
-const lightModeBtn = document.getElementById('lightModeBtn')
-const headAndFoot = document.querySelectorAll('#headAndFoot')
-const converter = document.querySelector('.converter')
+const lightModeBtn = document.getElementById('lightModeBtn');
+const headAndFoot = document.querySelectorAll('#headAndFoot');
+const converter = document.querySelector('.converter');
 
-lightModeBtn.addEventListener('click', themeSwitcher)
 
-async function themeSwitcher() {
+const clientCurrencyInp = document.getElementById('yourCurrency');
+const intoCurrencyInp = document.getElementById('intoCurrency');
+const clientValueInp = document.getElementById('clientValueInp');
+const resultValueInp = document.getElementById('resultValueInp');
+const host = 'api.frankfurter.app';
+
+let clientCurrency, intoCurrency, clientValue, resultValue, link;
+
+lightModeBtn.addEventListener('click', themeSwitcher);
+
+makeAUrl(clientCurrencyInp, intoCurrencyInp, clientValueInp)
+
+function themeSwitcher() {
     if (lightModeBtn.innerText === "light mode") {
         lightModeBtn.innerText = "dark mode"
         lightModeBtn.classList.remove('lightModeBtn')
@@ -30,4 +41,50 @@ async function themeSwitcher() {
         })
         converter.style.backgroundColor = 'var(--lightConverterBgColor)'
     }
+}
+
+function makeAUrl(clientCurrencyInp, intoCurrencyInp, clientValueInp) {
+    clientCurrencyInp.addEventListener('change', () => {
+        clientCurrency = clientCurrencyInp.value;
+        link = `https://${host}/latest?amount=${clientValue}&from=${clientCurrency}&to=${intoCurrency}`
+        dataAboutClientCurrency()
+    });
+    
+    intoCurrencyInp.addEventListener('change', () => {
+        intoCurrency = intoCurrencyInp.value;
+        link = `https://${host}/latest?amount=${clientValue}&from=${clientCurrency}&to=${intoCurrency}`
+        dataAboutClientCurrency()
+    });
+    
+    clientValueInp.addEventListener('change', () => {
+        clientValue = clientValueInp.value;
+        link = `https://${host}/latest?amount=${clientValue}&from=${clientCurrency}&to=${intoCurrency}`
+        dataAboutClientCurrency()
+    });
+}
+
+async function dataAboutClientCurrency() {
+    if (clientCurrency != undefined && 
+        intoCurrency != undefined &&
+        clientValue != undefined
+    ) {
+        try {
+            const res = await fetch(link);
+            const data = await res.json()
+            logResult(data, resultValue)
+        } catch (error) {
+            console.error(error)
+        } 
+    }
+    
+}
+
+function logResult(data, resultValue) {
+    if (clientCurrency != intoCurrency) {
+        resultValue = Object.values(data.rates)[0] 
+        resultValueInp.value = resultValue
+    } else {
+        resultValue = clientValue;
+        resultValueInp.value = resultValue
+    }   
 }
